7431ca054f21da267e84072cd5f3035a9838a4e9
CaoYX <1902818543@qq.com>
2022-03-04 20:47:13 +0800

    first commit for Homework03

---
+++ Homework03/Assignment1/Program.cs
@@ -0,0 +1,90 @@
+﻿//编写面向对象程序实现长方形、正方形、三角形等形状的类。每个形状类都能计算面积、判断形状是否合法。
+//请尝试合理使用接口/抽象类、属性来实现。
+
+using System;
+
+namespace Assignment1
+{
+    public interface Shape
+    {
+        bool IsValidShape();
+        double GetArea();
+    }
+    public class Rectangle:Shape
+    {
+        private readonly int width, height;        
+        public Rectangle(int width,int height)
+        {
+            if (width < 0 || height < 0)
+                throw new ArgumentException("Invalid width or height.");
+            this.width = width;
+            this.height = height;
+        }
+        public bool IsValidShape()
+        {
+            if(width>0&&height>0)
+                return true;    
+            return false;
+        }
+        public double GetArea()
+        {
+            if (!IsValidShape())
+                throw new Exception("Invalid width or height.");
+            return width*height;
+        }
+    }
+    public class Square : Rectangle
+    {
+        private int side;
+        public Square(int side):base(side,side)
+        {
+            if (side < 0) throw new ArgumentException("Invalid side.");
+            this.side = side;
+        }
+        new public bool IsValidShape()
+        {
+            if(side>0)return true;
+            else return false;  
+        }
+        new public double GetArea()
+        {
+            if(!IsValidShape())
+                throw new ArgumentException("Invalid side.");
+
+            return side *side;
+        }
+    }
+    public class Triangle : Shape
+    {
+        int line1,line2,line3;
+        public Triangle(int l1, int  l2, int l3)
+        {
+            if (l1<=0||l2<=0||l3<=0||l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1)
+                throw new ArgumentException("Invalid line length.");
+            line1 = l1;
+            line2 = l2;
+            line3 = l3;
+        }
+        public bool IsValidShape (){
+            if (line1 <= 0 || line2 <= 0 || line3 <= 0
+                || line1 + line2 <= line3
+                || line1 + line3 <= line2
+                || line2 + line3 <= line1)
+                return false;
+            return true;    
+        }
+        public double GetArea()
+        {
+            //Heron's formula
+            double s=(line1+line2+line3)/2;
+            return Math.Sqrt(s * (s - line1) * (s - line2) * (s - line3));
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Homework03/Assignment2/Program.cs
@@ -0,0 +1,101 @@
+﻿//Assignment2
+//随机创建10个形状对象，计算这些对象的面积之和。 尝试使用简单工厂设计模式来创建对象。
+
+using System;
+using Assignment1;
+using System.Collections.Generic;
+
+namespace Assignment2
+{
+    public class Factory
+    {
+        private static int shapeNum = 3;//可生成的形状种类
+        public Factory(int max,int min)
+        {
+            maxLength = max;
+            minLength = min;
+        }
+
+        //形状最大边长默认为20，最小边长默认为1；
+        public static int maxLength{ get;set;} = 20;
+        public static int minLength { get; set; } = 1;
+        Rectangle CreateRctangle()
+        {
+            Random random = new Random();
+            int width = random.Next(minLength,maxLength);
+            int height = random.Next(minLength, maxLength);
+            return new Rectangle(width, height);
+        }
+        Square CreateSquare()
+        {
+            Random random = new Random();
+            int side = random.Next(minLength, maxLength);
+            return new Square(side);
+        }
+        Triangle CreateTriangle()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int l1 = random.Next(minLength, maxLength), 
+                    l2 = random.Next(minLength, maxLength),
+                    l3 = random.Next(minLength, maxLength);
+                if (l1 <= 0 || l2 <= 0 || l3 <= 0)
+                    continue;
+                else if (l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1)
+                    continue;
+                else
+                    return new Triangle(l1, l2, l3);;
+            }            
+        }
+        public object CreateRandShape()
+        {
+            Random random = new Random();
+            int type=random.Next(1,shapeNum);
+            switch (type)
+            {
+                case 0:
+                    return CreateRctangle();
+                case 1: 
+                    return CreateSquare();
+                default://2
+                    return CreateTriangle();                
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> shapesList = new List<Object>();
+            Console.WriteLine("How many shapes do you want?");//10
+            int amount;
+            Factory factory = new Factory(10,1);//最大边长为10
+
+            try
+            {              
+                int.TryParse(Console.ReadLine(), out amount);
+            }catch(Exception e)
+            {
+                throw new Exception("Invalid input.");
+            }
+
+            //创建shape列表
+            for(int i=0; i < amount; i++)
+            {
+                shapesList.Add(factory.CreateRandShape());
+            }
+
+            double areaSum = 0;
+            Console.WriteLine("We have created these shapes:");
+            foreach(Shape shape in shapesList)
+            {
+                Console.WriteLine($"{shape.GetType()}");
+                areaSum += shape.GetArea();
+            }
+            Console.WriteLine("The sum of their area is {0}", areaSum);
+
+
+        }
+    }
+}
